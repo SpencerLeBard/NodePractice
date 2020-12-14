452a19bf07c6e8f095db456acd30e2ef26d0f1d5
SpencerLeBard <slebard@gmail.com>
2020-12-14 13:00:53 -0700

    express

--- app.js
+++ app.js
@@ -1,7 +1,23 @@
+//NOTE require fuctiuon manages dependancys of program (libraries), modules 
+// import http from 'http';
 var http = require('http');
 
+//creates server conatant by invoking createServer function on http module api
+//accepts arguments that is request listener function
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/html'});
-  res.end('Hello World!');
-}).listen(8080); 
-console.log("server is running");
+
+  //returning hello world with .end method
+
+  res.end('Server is Running ... ');
+  //NOTE create server only creates, need listen method to activate 
+}).listen(8080 , () => {
+  console.log("server is running");
+}); 
+
+
+//block scope created by curly braces {}, variables inside function do not leak if use "let/const", var leaks 
+
+//arrow functon does not care who calls it, great for events and listeners
+//normal function binds .this keyword to caller
+
---
+++ practice.js
@@ -0,0 +1,7 @@
+//NOTE function wraps every module...
+
+//function (exports, module, require, __filename, __dirname){
+// console.log(arguments)
+//returns module.exports
+//}
+

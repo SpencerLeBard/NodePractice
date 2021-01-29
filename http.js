00aa670a729da8d6be7d92ea7ad46239262e31a7
SpencerLeBard <slebard@gmail.com>
2021-01-29 11:36:40 -0700

    web server listening to port 3000

--- http.js
+++ http.js
@@ -1,2 +1,20 @@
 const http = require('http');
-const server = http.createServer();
+
+const server = http.createServer((req , res) => {
+  if(req.url === '/'){
+    res.write('Hello World');
+    res.end();
+  }
+  if(req.url === '/api/courses'){
+    res.write(JSON.stringify([1,2,3]))
+    res.end();
+  }
+});
+
+// server.on('connection' , () => {
+//   console.log('new connection...');
+// })
+
+server.listen(3000);
+
+console.log('listening on port 3000...');
